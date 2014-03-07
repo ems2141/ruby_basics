ee1858021b3d347b6e0af68c0cd69f853e6c81ba
Ellie Schneiders <ems2141@gmail.com>
2014-03-07 14:51:45 -0700

    created the first iteration of a program that sings 99 bottles of beer on the wall

---
+++ bin/beer_on_the_wall.rb
@@ -0,0 +1,20 @@
+puts "How many bottles of beer do you want me to sing?"
+
+bottles = 99
+
+
+while (bottles >= 0)
+    if (bottles == 2 )
+      puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer.\nTake one down and pass it around, #{bottles-1} bottle of beer on the wall.\n"
+    elsif(bottles == 1)
+      puts "#{bottles} bottle of beer on the wall, #{bottles} bottle of beer.\nTake one down and pass it around, no bottles of beer on the wall.\n"
+    elsif(bottles == 0)
+      puts "No bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+    else
+      puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer.\nTake one down and pass it around, #{bottles-1} bottles of beer on the wall.\n"
+    end
+    bottles -= 1
+end
+
+
+
