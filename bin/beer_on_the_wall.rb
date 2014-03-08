bbed90486612cba7d679f89437e4adb37138be24
Ellie Schneiders <ems2141@gmail.com>
2014-03-08 16:27:18 -0700

    Created a program that sings 99 bottles of beer on the wall after collecting user input on how many bottles of beer to start with

--- bin/beer_on_the_wall.rb
+++ bin/beer_on_the_wall.rb
@@ -1,14 +1,14 @@
-puts "How many bottles of beer do you want me to sing?"
+puts "I need to know how many bottles to sing! How many would you like?"
 
-bottles = 99
+bottles = Integer(gets.chomp)
 
 
-while (bottles >= 0)
-    if (bottles == 2 )
+while bottles >= 0
+    if bottles == 2
       puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer.\nTake one down and pass it around, #{bottles-1} bottle of beer on the wall.\n"
-    elsif(bottles == 1)
+    elsif bottles == 1
       puts "#{bottles} bottle of beer on the wall, #{bottles} bottle of beer.\nTake one down and pass it around, no bottles of beer on the wall.\n"
-    elsif(bottles == 0)
+    elsif bottles == 0
       puts "No bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
     else
       puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer.\nTake one down and pass it around, #{bottles-1} bottles of beer on the wall.\n"
