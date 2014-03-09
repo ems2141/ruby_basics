4031be8bb31c179a1a485b85991c7354c527d13b
Ellie Schneiders <ems2141@gmail.com>
2014-03-09 17:11:36 -0600

    Updated deaf_grandma
    I update this program to not try to convert a random number to an int because it already does this.  Also, I changed an else statement to an elsif statment to be able to pass a condition through.

--- bin/deaf_grandma.rb
+++ bin/deaf_grandma.rb
@@ -3,11 +3,11 @@
 # ask user to write something
 puts "please say something to your grandma!"
 greeting = gets.chomp
-year = (1930 + rand(21)).to_int
+year = 1930 + rand(21)
 
 #check string case
 if greeting =~ /[a-z]/
   puts "HUH?! SPEAK UP SONNY!"
-else greeting =~ /[A-Z]/
+elsif greeting =~ /[A-Z]/
   puts "NO, NOT SINCE #{year}!"
 end
